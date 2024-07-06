d3df00aff5606c55935030789cd070e7306beb25
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-07-06 18:37:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ app.get('/api/precos', (req, res) => {
   return res.json(produtos);
 });
 
+app.post('/api/precos', (req, res) => {
+
+  const novoProduto = {
+    id: produtos.length ? produtos[produtos.length - 1].id + 1 : 1,
+    produto: req.body.produto,
+    preco: req.body.preco
+  };
+  produtos.push(novoProduto);
+  res.status(201).json(novoProduto);
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`)
