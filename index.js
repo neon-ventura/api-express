9318db20c55c0d3e88e318228d433d29c4e41a58
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-07-06 10:25:37 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+const port = process.env.PORT || 3000
+
+app.get('/api/precos', (req, res) => {
+    const dataPath = path.join(__dirname, 'db.json');
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      if (err) {
+        return res.status(500).json({ error: 'Erro ao ler o arquivo' });
+      }
+      res.json(JSON.parse(data));
+    });
+  });
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`)
+})
