a1dcc3d93f82326ebcf435607d7f8d076845bdd6
Neon Ventura <123643824+neon-ventura@users.noreply.github.com>
2024-07-07 01:37:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/api/precos', (req, res) => {
 
 app.get('/api/precos/:id', (req, res) => {
   const id = req.params.id
-  res.json(produtos[id])
+  res.json(produtos[id + 1])
 })
 
 app.post('/api/precos', (req, res) => {
